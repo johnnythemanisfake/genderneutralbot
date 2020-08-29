3dab1a25a556c87c16726f857b9445847e91b3f2
johnnythemanisfake <70426003+johnnythemanisfake@users.noreply.github.com>
2020-08-29 14:23:18 +0800

    Add files via upload

---
+++ bot_main.py
@@ -0,0 +1,29 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+from bot_script import hxhx
+
+load_dotenv()
+TOKEN = os.getenv('bot_token')
+
+client = commands.Bot(command_prefix = '?')
+
+print('started\n')
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is here to police ur speech!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return 0
+    if message.content.startswith('?safe'):
+        print(message.content)
+        new_msg = hxhx(message.content)
+        print(new_msg)
+        await message.channel.send(new_msg)
+        print('done')
+
+client.run(TOKEN)
---
+++ bot_script.py
@@ -0,0 +1,25 @@
+def hxhx(message):
+#    message = message.replace('him', 'them')
+#    message = message.replace('his', 'their')
+#    message = message.replace('he', 'they')
+#    message = message.replace('she', 'they')
+    message = message.replace('?safe', '')
+    message = message.replace('?safe ', '')
+    
+    message = message.replace('dinuguan', 'bawal \'yan kapatid')
+    message = message.replace('pre-marital sex', 'deliks brad')
+    message = message.replace('pre marital sex', 'deliks brad')
+    message = message.replace('premarital sex', 'deliks brad')
+
+    message = message.replace('a', 'x')
+    message = message.replace('A', 'X')
+    message = message.replace('e', 'x')
+    message = message.replace('E', 'E')
+    message = message.replace('i', 'x')
+    message = message.replace('I', 'X')
+    message = message.replace('o', 'x')
+    message = message.replace('O', 'X')
+    message = message.replace('u', 'x')
+    message = message.replace('U', 'X')
+    
+    return message
